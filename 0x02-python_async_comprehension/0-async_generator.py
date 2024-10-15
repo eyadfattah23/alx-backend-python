0a364f7d53de564f162416037b28ce242e78a266
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-10-15 21:01:39 +0300

    use generato annotations instead of AsyncGenerator because of python support

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,11 +5,11 @@ Define a coroutine function called async_generator
         then yield a random number between 0 and 10.
 '''
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 import random
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """loop 10 times, each time asynchronously wait 1 second,
         then yield a random number
 
@@ -17,6 +17,6 @@ async def async_generator() -> AsyncGenerator[float, None]:
         Generator[float, None]: _description_
     """
 
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)  # Simulate a delay
         yield random.uniform(0, 10)
