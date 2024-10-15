5c5356c77ae320329220008c82aefdb5cc2d70da
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-10-15 18:24:53 +0300

    use pythonic syntax

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -20,9 +20,5 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
         List[float]: the list of all the delays,
                 order without using sort() because of concurrency.
     """
-    tasks = []
-    for _ in range(n):
-        task = task_wait_random(max_delay)
-        tasks.append(task)
-
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
