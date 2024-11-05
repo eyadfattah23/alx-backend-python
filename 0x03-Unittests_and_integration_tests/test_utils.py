f884519678fe5f020bb4537ca52994a393ff5509
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-11-05 20:41:21 +0200

    create mock tests for get_json func.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,10 +5,12 @@ Create a TestAccessNestedMap unit test for utils.access_nested_map
 
 from parameterized import parameterized
 from unittest import TestCase
-from utils import access_nested_map
+from unittest.mock import Mock, patch
+from utils import access_nested_map, get_json
 
 from typing import (
     Mapping,
+    Callable,
     Sequence
 )
 
@@ -41,3 +43,38 @@ class TestAccessNestedMap(TestCase):
         """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(TestCase):
+    """test that utils.get_json returns the expected result."""
+
+    @patch('requests.get')
+    def test_get_json(self, mock_get_json: Callable):
+        """test that utils.get_json returns the expected result.
+
+        Args:
+            url (_type_): _description_
+            mock_get_json (_type_): _description_
+        """
+        mock_response = Mock()
+        test_url = "http://example.com"
+        test_payload = {"payload": True}
+
+        mock_response.json.return_value = test_payload
+        mock_get_json.return_value = mock_response
+
+        data = get_json(test_url)
+        mock_get_json.assert_called_with(test_url)
+
+        self.assertEqual(data, test_payload)
+
+        test_url = "http://holberton.io"
+        test_payload = {"payload": False}
+
+        mock_response.json.return_value = test_payload
+        mock_get_json.return_value = mock_response
+
+        data = get_json(test_url)
+        mock_get_json.assert_called_with(test_url)
+
+        self.assertEqual(data, test_payload)
