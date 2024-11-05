7764bb82928771a9d712ef2d386b9a411ef46b1b
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-11-05 20:00:51 +0200

    test the error raising for the access_nested_map

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -26,3 +26,18 @@ class TestAccessNestedMap(TestCase):
                                path: Sequence, expected: int):
         """test if access_nested_map returns what it is supposed to"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a,")),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map: Mapping,
+                                         path: Sequence):
+        """ test that a KeyError is raised with prev inputs
+
+        Args:
+            nested_map (Mapping)
+            path (Sequence)
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
