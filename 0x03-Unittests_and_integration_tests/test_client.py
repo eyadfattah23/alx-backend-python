335fab716e435c54b650417d3971f7551f95f425
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-11-08 00:48:18 +0200

    Use patch as a context manager to patch GithubOrgClient.org and make it return a known payload.
    Test that the result of _public_repos_url is the expected one based on the mocked payload.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 '''Unittests for client file'''
 from parameterized import parameterized
 from unittest import TestCase
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, PropertyMock
 from client import GithubOrgClient
 from utils import get_json
 from typing import (Callable)
@@ -36,3 +36,18 @@ class TestGithubOrgClient(TestCase):
                              client.org)
         mock_org.assert_called_once_with(
             ''"https://api.github.com/orgs/{}".format(org))
+
+    def test_public_repos_url(self):
+        """test _public_repos_url property in GithubOrgClient.
+        """
+
+        with patch.object(GithubOrgClient, 'org',
+                          new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = {
+                "repos_url": "mock.github.com/repos",
+                "login": "google",
+                "id": 1342004,
+                "is_verified": True
+            }
+            inst = GithubOrgClient('google')
+            self.assertEqual("mock.github.com/repos", inst._public_repos_url)
